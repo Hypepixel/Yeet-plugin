3210a237490732a9d79236939140c17cd1f0ac58
JasperDre <jasperdre@gmail.com>
2018-09-22 16:11:33 +0200

    Added messages and custom config

--- Yeet/src/main/java/stevekeef/yeet/main/CommandKit.java
+++ Yeet/src/main/java/stevekeef/yeet/main/CommandKit.java
@@ -1,39 +1,70 @@
 package stevekeef.yeet.main;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class CommandKit implements CommandExecutor
 {
+	private File customConfigFile;
+	private FileConfiguration config;
+	
 	// This method is called, when somebody uses our command
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
     {
     	if (sender instanceof Player)
     	{
+    		customConfigFile = Yeet.getPlugin(Yeet.class).getCustomConfigFile();
+    		config = Yeet.getPlugin(Yeet.class).getCustomConfig();
             Player player = (Player) sender;
             
             if (player.getInventory().contains(Material.DIAMOND) == false)
             {
+            	if (config.contains(player.getUniqueId().toString()) == false)
+            	{
 	            // Create a new ItemStack (type: diamond)
 	            ItemStack diamond = new ItemStack(Material.DIAMOND, 1);
 	
 	            // Give the player our items (comma-seperated list of all ItemStack)
 	            player.getInventory().addItem(diamond);
 	            
+	            // Add message
+	            player.sendMessage("Yeet!");
+	            
 	            // Player sound-effect
 	            player.playSound(player.getEyeLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 1);
+	            
+	            config.set("Data.player", player.getUniqueId().toString());
+	            
+	            try {
+					config.save(customConfigFile);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            	}
+            	else
+            	{
+            		Bukkit.getConsoleSender().sendMessage("You've already yeeted!");
+            	}
             }
         }
     	else
     	{
     		// Send error handling to the entity
-    		sender.sendMessage("You have to be a player to yeet");
+    		Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "You need to be a player to yeet");
     		
     		return false;
     	}
--- Yeet/src/main/java/stevekeef/yeet/main/Yeet.java
+++ Yeet/src/main/java/stevekeef/yeet/main/Yeet.java
@@ -1,13 +1,24 @@
 package stevekeef.yeet.main;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Yeet extends JavaPlugin
 {
+	private File customConfigFile;
+	private FileConfiguration customConfig;
+	
 	// Fired when plugin is first enabled
     @Override
     public void onEnable()
     {
+    	createCustomConfig();
+    	
     	// Register our command "yeet" (set an instance of your command class as executor)
         this.getCommand("yeet").setExecutor(new CommandKit());
     }
@@ -17,5 +28,33 @@ public class Yeet extends JavaPlugin
     public void onDisable()
     {
     	
+    }
+    
+    public FileConfiguration getCustomConfig()
+    {
+        return this.customConfig;
+    }
+    
+    public File getCustomConfigFile()
+    {
+    	return this.customConfigFile;
+    }
+    
+    private void createCustomConfig()
+    {
+        customConfigFile = new File(getDataFolder(), "yeet.yml");
+        if (!customConfigFile.exists())
+        {
+            customConfigFile.getParentFile().mkdirs();
+            saveResource("yeet.yml", false);
+         }
+
+        customConfig= new YamlConfiguration();
+        
+        try {
+            customConfig.load(customConfigFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
     }
 }
