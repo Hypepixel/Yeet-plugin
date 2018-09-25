82fa47a66f14971e62bdd97a9bd6c86ac7870882
JasperDre <jasperdre@gmail.com>
2018-09-25 10:15:00 +0200

    Added date format and count with default config

--- Yeet/src/main/java/stevekeef/yeet/main/CommandKit.java
+++ Yeet/src/main/java/stevekeef/yeet/main/CommandKit.java
@@ -1,24 +1,26 @@
 package stevekeef.yeet.main;
 
-import java.io.File;
-import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 
 import net.md_5.bungee.api.ChatColor;
 
 public class CommandKit implements CommandExecutor
 {
-	private File customConfigFile;
-	private FileConfiguration config;
+	private Plugin plugin = Yeet.getPlugin(Yeet.class);
+	
+	private int count = 0;
 	
 	// This method is called, when somebody uses our command
     @Override
@@ -26,54 +28,109 @@ public class CommandKit implements CommandExecutor
     {
     	if (sender instanceof Player)
     	{
-    		customConfigFile = Yeet.getPlugin(Yeet.class).getCustomConfigFile();
-    		config = Yeet.getPlugin(Yeet.class).getCustomConfig();
             Player player = (Player) sender;
             
-            if (player.getInventory().contains(Material.DIAMOND) == false)
+            Calendar calendarNow = Calendar.getInstance();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            
+            if (plugin.getConfig().getString("users." + player.getUniqueId() + ".date") != null)
             {
-            	if (player.getUniqueId().toString().equals(config.getString("player")) == false)
+            	Calendar calendarLoaded = Calendar.getInstance();
+            	
+            	try
             	{
-	            // Create a new ItemStack (type: diamond)
-	            ItemStack diamond = new ItemStack(Material.DIAMOND, 1);
-	
-	            // Give the player our items (comma-separated list of all ItemStack)
-	            player.getInventory().addItem(diamond);
-	            
-	            // Send message
-	            player.sendMessage("Yeet!");
-	            
-	            // Player sound-effect
-	            player.playSound(player.getEyeLocation(), Sound.BLOCK_NOTE_BLOCK_BASS, 1, 1);
-	            
-	            config.set("player", player.getUniqueId().toString());
-	            
-	            try {
-					config.save(customConfigFile);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					calendarLoaded.setTime(dateFormat.parse(plugin.getConfig().getString("users." + player.getUniqueId() + ".date")));
 				}
+            	catch (ParseException e)
+            	{
+					e.printStackTrace();
+					Bukkit.getConsoleSender().sendMessage(ChatColor.RED + e.toString());
+				}
+            	
+				int deltaHours = calendarNow.get(Calendar.HOUR) - calendarLoaded.get(Calendar.HOUR);
+            		
+            	if (plugin.getConfig().getString("users." + player.getUniqueId() + ".count") != null)
+            	{
+            		if (deltaHours >= 1)
+            		{
+            			// Upgrade the yeet
+	            		count = plugin.getConfig().getInt("users." + player.getUniqueId() + ".count");
+	            		itemEvent(player, count);
+	            		count++;
+	            		
+	            		saveUser(player, dateFormat.format(calendarNow.getTime()), count);
+            		}
+            		else
+            		{
+            			// On cooldown
+            			player.sendMessage("Cooldown! Wait an hour to flex more!");
+            			Bukkit.getConsoleSender().sendMessage(Integer.toString(deltaHours));
+            		}
             	}
             	else
             	{
-            		player.sendMessage("You've already yeeted!");
+            		// Yeet for the first time today
+            		itemEvent(player, count);
+            		count = 1;
+            		
+            		saveUser(player, dateFormat.format(calendarNow.getTime()), count);
             	}
             }
             else
-        	{
-        		player.sendMessage("You are already flexing!");
-        	}
+            {
+            	// Yeet for the first time
+        		itemEvent(player, count);
+	            count = 1;
+	            
+	            saveUser(player, dateFormat.format(calendarNow.getTime()), count);
+            }
         }
     	else
     	{
     		// Send error handling to the entity
     		Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "You need to be a player to yeet");
-    		
-    		return false;
     	}
     	
     	// If the player (or console) uses our command correct, we can return true
         return true;
+    }
+    
+    private void itemEvent(Player player, int itemCount)
+    {
+        switch(itemCount)
+        {
+        	case 0:
+        		ItemStack iron = new ItemStack(Material.IRON_INGOT, 1);
+        		player.getInventory().addItem(iron);
+        		player.sendMessage("Yeet!");
+        		player.playSound(player.getEyeLocation(), Sound.BLOCK_NOTE_BLOCK_BASS, 1, 1);
+        		Bukkit.broadcastMessage(player.getDisplayName() + " just flexed!");
+        	break;
+        	case 1:
+        		ItemStack gold = new ItemStack(Material.GOLD_INGOT, 1);
+			   player.getInventory().addItem(gold);
+			   player.sendMessage("Yeet!");
+			   player.playSound(player.getEyeLocation(), Sound.BLOCK_NOTE_BLOCK_BASS, 1, 1);
+			   Bukkit.broadcastMessage(player.getDisplayName() + " just flexed!");
+		      break;
+        	case 2:
+        		ItemStack diamond = new ItemStack(Material.DIAMOND, 1);
+			   player.getInventory().addItem(diamond);
+			   player.sendMessage("Yeet!");
+			   player.playSound(player.getEyeLocation(), Sound.BLOCK_NOTE_BLOCK_BASS, 1, 1);
+			   Bukkit.broadcastMessage(player.getDisplayName() + " just flexed!");
+		      break;
+        	default :
+			   player.sendMessage("You are already flexing!");
+			   break;
+        }
+    }
+    
+    private void saveUser(Player player, CharSequence date, int itemCount)
+    {
+    	// Set and save user
+        plugin.getConfig().set("users." + player.getUniqueId() + ".date", date);
+        plugin.getConfig().set("users." + player.getUniqueId() + ".count", itemCount);
+        plugin.saveConfig();
     }
 }
--- Yeet/src/main/java/stevekeef/yeet/main/Yeet.java
+++ Yeet/src/main/java/stevekeef/yeet/main/Yeet.java
@@ -1,25 +1,17 @@
 package stevekeef.yeet.main;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.bukkit.configuration.InvalidConfigurationException;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Yeet extends JavaPlugin
 {
-	private File customConfigFile;
-	private FileConfiguration customConfig;
-	
 	// Fired when plugin is first enabled
     @Override
     public void onEnable()
     {
-    	createCustomConfig();
+    	loadConfig();
     	
     	// Register our command "yeet" (set an instance of your command class as executor)
+    	getServer().getConsoleSender().sendMessage("Setting commands");
         this.getCommand("yeet").setExecutor(new CommandKit());
     }
     
@@ -30,31 +22,13 @@ public class Yeet extends JavaPlugin
     	
     }
     
-    public FileConfiguration getCustomConfig()
+    private void loadConfig()
     {
-        return this.customConfig;
-    }
-    
-    public File getCustomConfigFile()
-    {
-    	return this.customConfigFile;
-    }
-    
-    private void createCustomConfig()
-    {
-        customConfigFile = new File(getDataFolder(), "custom.yml");
-        if (!customConfigFile.exists())
-        {
-            customConfigFile.getParentFile().mkdirs();
-            saveResource("custom.yml", false);
-         }
-
-        customConfig= new YamlConfiguration();
-        
-        try {
-            customConfig.load(customConfigFile);
-        } catch (IOException | InvalidConfigurationException e) {
-            e.printStackTrace();
-        }
+    	getServer().getConsoleSender().sendMessage("Loading config");
+    	
+    	getConfig().options().copyDefaults(true);
+    	saveConfig();
+    	
+    	getServer().getConsoleSender().sendMessage("Loaded config");
     }
 }
